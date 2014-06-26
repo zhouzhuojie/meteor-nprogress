1ce413c65768675d6ca8a6b4cd3e5e6ce79b9faf
zhouzhuojie <zhouzhuojie@gmail.com>
2014-06-26 13:17:56 -0400

    Add laika tests, update nprogress version

--- package.js
+++ package.js
@@ -4,9 +4,9 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.use('jquery', 'client');
-  api.add_files('nprogress/nprogress.js', 'client');
-  api.add_files('nprogress/nprogress.css', 'client');
-  api.add_files('main.js', 'client');
+  api.add_files('lib/nprogress/nprogress.js', 'client');
+  api.add_files('lib/nprogress/nprogress.css', 'client');
+  api.add_files('lib/main.js', 'client');
   if (api.export){
     api.export('NProgress', 'client');
   }
