d27a202afee947498c947c780bc18efe8ec0c1e4
zhouzhuojie <zhouzhuojie@gmail.com>
2015-05-20 16:20:01 -0400

    0.2.0

--- package.js
+++ package.js
@@ -1,27 +1,19 @@
 Package.describe({
-  summary: "NProgress for Meteor",
-  version: "0.1.0",
-  git: "https://github.com/zhouzhuojie/meteor-nprogress.git",
-  name: "mrt:nprogress"
+    summary: "NProgress for Meteor",
+    version: "0.2.0",
+    git: "https://github.com/zhouzhuojie/meteor-nprogress.git",
+    name: "mrt:nprogress"
 });
 
 Package.onUse(function (api) {
-  api.use('jquery@1.0.0', 'client');
-  api.add_files('lib/nprogress/nprogress.js', 'client');
-  api.add_files('lib/nprogress/nprogress.css', 'client');
-  api.add_files('lib/main.js', 'client');
-  if (api.export){
+    api.use('jquery@1.0.0', 'client');
+    api.add_files('lib/nprogress/nprogress.js', 'client');
+    api.add_files('lib/nprogress/nprogress.css', 'client');
+    api.add_files('lib/main.js', 'client');
     api.export('NProgress', 'client');
-  }
 });
 
-if (Package.on_test) {
-  Package.on_test(function (api) {
-    if (Package.onTest) {
-      api.use(['mrt:nprogress@0.1.0', 'tinytest', 'test-helpers'], ['client']);
-    } else {
-      api.use(['nprogress', 'tinytest', 'test-helpers'], ['client']);
-    }
-    api.add_files('test-mrt:nprogress.js', ['client']);
-  });
-}
+Package.onTest(function (api) {
+    api.use(['mrt:nprogress', 'tinytest'], 'client');
+    api.add_files('test-mrt:nprogress.js', 'client');
+});
